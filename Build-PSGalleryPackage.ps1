9b7de9014267acaeb046281d5c22ad6b487a4bfc
Tony Cavella <acavella@gmail.com>
2020-01-29 19:11:09 -0500

    Update .gitignore Update Build

---
+++ Build-PSGalleryPackage.ps1
@@ -0,0 +1,19 @@
+﻿$VerbosePreference = 'Continue'
+$ErrorActionPreference = 'Stop'
+$baseDir = $PSScriptRoot
+
+try {
+    $buildDir = "$baseDir\build\psgallery\PSOfflineFOD"
+    $null = New-Item -Path $buildDir -ItemType Directory -Verbose
+
+    Write-Verbose "Copying release files to build folder '$buildDir'"
+    Copy-Item $baseDir\PSOfflineFOD.psd1    $buildDir\
+    Copy-Item $baseDir\PSOfflineFOD.psm1    $buildDir\
+
+    Write-Verbose 'Copy complete. Contents:'
+    Get-ChildItem $buildDir -Recurse | Out-Host
+}
+catch {
+    Write-Error -ErrorRecord $_
+    exit 1
+}
